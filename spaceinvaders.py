0f02921f21c3600ce04b5c4e04b588e41db4bbc1
YikeDM <davidmartin6112000@gmail.com>
2023-08-29 23:13:54 +0100

    I lied, I'm updating

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -10,17 +10,26 @@ class Bullet:
         self.state = 1
         self.speed = 15
         self.image = pygame.image.load("bullet.bmp")
-        self.rect = self.image.get_rect(center = (self.x + 50, self.y))
+        self.rect = self.image.get_rect(center = (self.x+35, self.y))
+        self.rect = self.rect.inflate(-25, -35)
+        
+        
 
     def movebullet(self):
         if self.state:
             for i, x in enumerate(enemy):
-                if self.rect.colliderect(enemy[i].rect):
+                #pygame.draw.rect(screen, (255,0,0), self.rect, 2)
+                #test draw hitbox ^
+                if self.rect.colliderect(enemy[i].hit_box):
                     enemy[i].ishit()
                     self.state = 0
-
+            
             
             self.rect[1] -= 15
+        
+    # TODO (##) Add trash collection for bullets? right now they stack up in the list.
+    #it shouldn't be an issue due to the game's size but it's definitely not the best
+    #practice.
 
     def initialize(self):
         if self.state:
@@ -45,16 +54,22 @@ class Player:
     
     def move(self, a):
         if a == 1:
-            if self.x > 0 and self.x < 1000:
+            if self.x > 0:
+                if self.x < 0:
+                    self.x = 50
                 self.x -= self.speed
             else:
-                self.x += self.speed*2
+                self.x = 50
+                #self.x += self.speed*2
 
         if a == 0:
-            if self.x > 0 and self.x < 1000:
+            if self.x < 1000:
+                if self.x > 1000:
+                    self.x = 950
                 self.x += self.speed
             else:
-                self.x -= self.speed*2
+                self.x = 950
+                #self.x -= self.speed*2
 
 class Alien:
     
@@ -62,11 +77,17 @@ class Alien:
         self.x = x
         self.y = y
         self.health = 5
-        self.hit_box = [self.x, self.y, 100, 50]
+        self.speed = 10
+        self.hit_box = [self.x, self.y-45, 90, 40]
         self.state = 1
         self.image = pygame.image.load("alien.bmp")
         self.rect = self.image.get_rect(center = (self.x + 50, self.y))
-    
+        self.originalx = x
+        self.originaly = y
+
+    def reset(self):
+        self.__init__(self.originalx, self.originaly)
+
     def ishit(self):
         self.health -= 2.5
 
@@ -84,9 +105,12 @@ class Alien:
             #move down randomly towards player
             if random.randint(1, 40) == 1:
                 move_enemy()
-            pygame.draw.rect(screen, (255,0,0), self.hit_box, 2)
+            #pygame.draw.rect(screen, (255,0,0), self.hit_box, 2)
+            #test draw hitbox^
         else:
             self.image = 0
+            self.rect.center = (0,0)
+            self.hit_box = [0,0,0,0]
             #draw visible hitbox
 
 def endgame(a):
@@ -105,16 +129,15 @@ def endgame(a):
         pygame.quit()
         sys.exit()
 
-def updatesprites():
-    temp = 0
+
 
 #moves enemy, enemy.y is obsolete as using rect to blit
 def move_enemy():
     global enemy
     for i, x in enumerate(enemy):
-        enemy[i].y += 10
-        enemy[i].rect[1] += 10
-        enemy[i].hit_box[1] += 10
+        enemy[i].y += enemy[i].speed
+        enemy[i].rect[1] += enemy[i].speed
+        enemy[i].hit_box[1] += enemy[i].speed
         if enemy[i].y > 600:
             endgame(0)
 
@@ -129,8 +152,7 @@ background = pygame.image.load("background.png")
 winner = pygame.image.load("winner.png")
 loser = pygame.image.load("loser.png")
 previous_time = pygame.time.get_ticks()
-
-
+font = pygame.font.SysFont("Liberation Mono", 40)
 while 1:
     game = 0
     for event in pygame.event.get():
@@ -151,7 +173,10 @@ while 1:
         Alien(600, 30), Alien(700, 30), Alien(800,30), Alien(900,30)]
 
         bullet_list = []
-
+        rounds = 0
+        scoredisplay = font.render(str(rounds+1), 1, (255,255,255))
+        scorestring = font.render("Round: ", 1, (255,255,255))
+        outof = font.render(" /10", 1, (255,255,255))
         player1 = Player(540, 600)  
         while game:
 
@@ -163,7 +188,7 @@ while 1:
                     
                     if event.key == pygame.K_SPACE:
                         current_time = pygame.time.get_ticks()
-                        if current_time - previous_time > 1000:
+                        if current_time - previous_time > 750:
                             player1.fire()
                             previous_time = current_time
 
@@ -176,7 +201,9 @@ while 1:
 
 
             screen.blit(background, (0, 0))
-
+            screen.blit(scorestring, (15, 15))
+            screen.blit(scoredisplay, (165, 15))
+            screen.blit(outof, (190,15))
             count1 = 0
             for i, x in enumerate(enemy):
                 enemy[i].initialize()
@@ -186,16 +213,26 @@ while 1:
         
 
             if count1 == 0:
-                endgame(1)
+                if rounds == 9:
 
-            for i, x in enumerate(bullet_list):
-                bullet_list[i].movebullet()
-                bullet_list[i].initialize()
+                    endgame(1)
+                else:
+                    rounds += 1
+                    scoredisplay = font.render(str(rounds + 1), 1, (255,255,255))
+                    for i, x in enumerate(enemy):
+                        enemy[i].reset()
+                        enemy[i].speed += rounds-1
+
+
 
             player1.initialize()
 
             for Alien in enemy:
                 Alien.update()
+            
+            for i, x in enumerate(bullet_list):
+                bullet_list[i].movebullet()
+                bullet_list[i].initialize()
             
             pygame.display.flip()
             pygame.time.wait(30)
